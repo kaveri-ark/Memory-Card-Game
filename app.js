5f2a26b90437ff04e839355f0e56b26c598759f4
Kaveri_Arkasali <68460091+kaveri-ark@users.noreply.github.com>
2021-04-21 11:51:04 +0530

    update

---
+++ app.js
@@ -0,0 +1,58 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let cardIsFlipped = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+  if (!cardIsFlipped) {
+    cardIsFlipped = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatched = firstCard.dataset.name === secondCard.dataset.name;
+  isMatched ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unFlipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [cardIsFlipped, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+
+(function shuffle() {
+  cards.forEach(function (card) {
+    let randomPositions = Math.floor(Math.random() * 12);
+    card.style.order = randomPositions;
+  });
+})();
+
+cards.forEach(function (card) {
+  card.addEventListener("click", flipCard);
+});
